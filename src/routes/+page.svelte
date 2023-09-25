1ce750fbac8d93aa765da90c54cb390f77bcce1a
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-09-25 15:47:21 +0800

    chore: update page layout and styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-gray-100 h-screen w-screen">
+  <h1>
+    <span class="line-through text-5xl">Portfolio</span>
+    <span class="text-5xl">Playground</span>
+  </h1>
+
+  <div class="flex flex-col gap-4 px-8 py-4 bg-white w-96">
+    <div class="text-xl">Login</div>
+    <div>
+      <label for="account">Account</label>
+      <input name="account" />
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input name="password" />
+    </div>
+  </div>
+</div>
