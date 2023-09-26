37a33734ac3d87ee8226cd1bda56405c309a7e51
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-09-26 14:56:13 +0800

    style: update page styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,25 @@
-<div class="bg-gray-100 h-screen w-screen">
+<div class="bg-neutral-300 h-screen w-screen">
   <h1>
     <span class="line-through text-5xl">Portfolio</span>
     <span class="text-5xl">Playground</span>
   </h1>
 
-  <div class="flex flex-col gap-4 px-8 py-4 bg-white w-96">
-    <div class="text-xl">Login</div>
-    <div>
-      <label for="account">Account</label>
-      <input name="account" />
-    </div>
-    <div>
-      <label for="password">Password</label>
-      <input name="password" />
+  <div class="p-4 bg-neutral-100 w-fit">
+    <div class="pl-4 bg-white w-96 rounded-lg divide-y">
+      <div class="pr-4 py-3 text-lg">
+        <label class="inline-block w-24" for="account">Account</label>
+        <input
+          name="account"
+          class="appearance-none focus:outline-none caret-blue-400"
+        />
+      </div>
+      <div class="pr-4 py-3 text-lg">
+        <label class="inline-block w-24" for="password">Password</label>
+        <input
+          name="password"
+          class="appearance-none focus:outline-none caret-blue-400"
+        />
+      </div>
     </div>
   </div>
 </div>
